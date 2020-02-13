27f02830ae34d926d44e98b6fa7aa97ad1854d75
David Ospina <jdospin@hotmail.com>
2020-02-12 22:55:43 -0500

    Added men section

--- src/homepage.component.jsx
+++ src/homepage.component.jsx
@@ -19,6 +19,12 @@ const HomePage = () => {
                 </div>
                 <div className='menu-item'>
                     <div className='content'>
+                        <h1 className='title'>SHOES</h1>
+                        <span className='subtitle'>SHOP NOW</span>
+                    </div>
+                </div>
+                <div className='menu-item'>
+                    <div className='content'>
                         <h1 className='title'>WOMENS</h1>
                         <span className='subtitle'>SHOP NOW</span>
                     </div>
