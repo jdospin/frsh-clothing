d334537bbf62ab06f91498f114bec8e06106a66e
David Ospina <jdospin@hotmail.com>
2020-05-15 17:24:48 -0400

    Introduced styled components in the app. Changed the custom button component and the homepage component to use styled components. Performed some code cleanup

--- src/components/sign-up/sign-up.component.jsx
+++ src/components/sign-up/sign-up.component.jsx
@@ -3,7 +3,6 @@ import FormInput from '../form-input/form-input.component';
 import CustomButton from '../custom-button/custom-button.component';
 import { auth, createUserProfileDocument } from '../../firebase/firebase.utils';
 import './sign-up.styles.scss';
-import SignIn from '../sign-in/sign-in.component';
 
 class SignUp extends React.Component {
   constructor() {
