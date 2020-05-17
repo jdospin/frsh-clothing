101ae4ed106c409605da3c3620500a60aea3e5e0
David Ospina <jdospin@hotmail.com>
2020-05-17 19:27:19 -0400

    Added routing to the collection preview component

--- src/components/collection-preview/collection-preview.component.jsx
+++ src/components/collection-preview/collection-preview.component.jsx
@@ -1,11 +1,12 @@
 import React from 'react'; 
+import { withRouter } from 'react-router-dom';
 import CollectionItem from '../collection-item/collection-item.component';
 import './collection-preview.styles.scss';
 
-const CollectionPreview = ({ title, items }) => {
+const CollectionPreview = ({ title, items, history, match, routeName }) => {
   return (
     <div className='collection-preview'>
-      <h1 className='title'>{title.toUpperCase()}</h1>
+      <h1 className='title' onClick={() => history.push(`${match.path}/${routeName}`)}>{title.toUpperCase()}</h1>
       <div className='preview'>
         {items
           .filter((item, idx) => idx < 4)
@@ -20,4 +21,4 @@ const CollectionPreview = ({ title, items }) => {
   );
 }
 
-export default CollectionPreview;
+export default withRouter(CollectionPreview);
