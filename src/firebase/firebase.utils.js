aef8e9f16d60b03ee225335f196517dc3d1ab683
David Ospina <jdospin@hotmail.com>
2020-05-17 17:31:28 -0400

    Added shop actions & shop types to redux. Implemented collections import from firestore and updating shop data in redux store from the imported data

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -62,8 +62,11 @@ export const convertCollectionsSnapshotToMap = (collections) => {
     };
   });
 
-  console.log(transformedCollection);
-}
+  return transformedCollection.reduce((accumulator, collection) => {
+    accumulator[collection.title.toLowerCase()] = collection;
+    return accumulator;
+  }, {});
+};
 
 export const auth = firebase.auth();
 export const firestore = firebase.firestore();
