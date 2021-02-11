d973d5c2e92c854fbdd299f1d8d823b665220797
David Ospina <jdospin@hotmail.com>
2021-02-11 13:50:44 -0500

    Add Saga to sign in user automatically after signing up

--- src/redux/shop/shop.sagas.js
+++ src/redux/shop/shop.sagas.js
@@ -1,9 +1,4 @@
-import {
-  takeLatest,
-  call,
-  put
-} from 'redux-saga/effects';
-
+import { takeLatest, call, put, all } from 'redux-saga/effects';
 import ShopActionTypes from './shop.types';
 import {
   firestore,
@@ -41,4 +36,8 @@ export function* fetchCollectionsAsync() {
 
 export function* fetchCollectionsStart() {
   yield takeLatest(ShopActionTypes.FETCH_COLLECTIONS_START, fetchCollectionsAsync);
+}
+
+export function* shopSagas() {
+  yield all([call(fetchCollectionsStart)]);
 }
